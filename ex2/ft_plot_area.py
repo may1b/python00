54948e7f7469461d26a0527ac3893b0224412092
May <magrass@student.42heilbronn.de>
2026-05-24 20:10:40 +0200

    done ex2!

---
+++ ex2/ft_plot_area.py
@@ -0,0 +1,6 @@
+def ft_plot_area():
+    print("Enter length: ", end='')
+    plot_len = int(input())
+    print("Enter width: ", end='')
+    plot_w = int(input())
+    print(f"Plot area: {(plot_len * plot_w)}")
