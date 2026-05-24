667ae25995b4b81e5404773e9113c212fe180a8c
May <magrass@student.42heilbronn.de>
2026-05-24 21:07:11 +0200

    fixed flake8 complaints

--- ex3/ft_harvest_total.py
+++ ex3/ft_harvest_total.py
@@ -1,5 +1,8 @@
 def ft_harvest_total():
-    print("Day 1 harvest: ",end=''); day1 = int(input())
-    print("Day 2 harvest: ",end=''); day2 = int(input())
-    print("Day 3 harvest: ",end=''); day3 = int(input())
+    print("Day 1 harvest: ", end='')
+    day1 = int(input())
+    print("Day 2 harvest: ", end='')
+    day2 = int(input())
+    print("Day 3 harvest: ", end='')
+    day3 = int(input())
     print(f"Total harvest: {day1 + day2 + day3}")
